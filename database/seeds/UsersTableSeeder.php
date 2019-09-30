6852d89cb48a1a8144cee8ecdbdec12866423029
saynowithme <saynowithme@gmail.com>
2019-10-01 01:16:41 +0700

    display all post to index

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //reset the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        // generate 3 users/author
+        DB::table('users')->insert([
+            [
+                'name' => "Đặng Trường Trúc",
+                'email' => "truongtrucit@gmail.com",
+                'password' => bcrypt('secret')
+
+            ],
+            [
+                'name' => "Mai Tố Uyên",
+                'email' => "maitouyenhanu@gmail.com",
+                'password' => bcrypt('secret')
+
+            ],
+            
+        ]);
+    }
+}
